2ca57833337eb1f462c03cc358315e6c0bc5169b
Hao Le <hao.le1110@hcmut.edu.vn>
2023-09-26 00:47:22 +0700

    Add Manage Customer Function

--- quanlikhachsan/main.cpp
+++ quanlikhachsan/main.cpp
@@ -6,6 +6,8 @@
 #include <time.h>
 
 using namespace std;
+
+
 //---------------------------------ENUM------------------------------------
 typedef enum{
     EXIT                    = 0,
@@ -92,6 +94,27 @@ public:
         this->address = customerAddress;
         this->idRoom = customerIdRoom;
     };
+    string getName(){
+        return this->name;
+    }
+    string getPhoneNumber(){
+        return this->phoneNumber;
+    }
+    string getAddress(){
+        return this->address;
+    }
+    int getIDRoom(){
+        return this->idRoom;
+    }
+    void setName(string name){
+        this->name = name;
+    }
+    void setPhoneNumber(string phoneNumber){
+        this->phoneNumber = phoneNumber;
+    }
+    void setAddress(string address){
+        this->address = address;
+    }
 };
 
 
@@ -125,11 +148,118 @@ class EmployeeManager{
         vector<Employee>& getdatabaseEmployee(){return databaseEmployee;}
 };
 
+//-----------------------------DECLARE FUNCTION--------------------------------
+void manageCustomer(vector<Room>& databaseRoom, vector<Customer>& databaseCustomer);
+
 //---------------------------------FUNCTION------------------------------------
+void editInfoCustomer(vector<Room>& databaseRoom, vector<Customer> databaseCustomer){
+    system("cls");
+    int stt;
+    int key, key1;
+    bool flag = 0;
+    bool continueEdit = 1;
+    string temp;
+    cout << "Nhap STT khach hang: ";
+    cin >> stt;
+    cout << "1:Sua ten\n2:Sua sdt\n3:Sua dia chi\n0: Quay lai\n\n";
+    while (continueEdit){
+        cout << "Nhap lua chon: ";
+        cin >> key;
+        switch (key)
+        {
+        case 1:
+            system("cls");
+            cout << "Nhap ten: ";
+            cin.ignore();
+            getline(cin, temp);
+            databaseCustomer[stt-1].setName(temp);
+            flag = 1;
+            break;
+        case 2:
+            system("cls");
+            cout << "Nhap sdt: ";
+            cin.ignore();
+            getline(cin, temp);
+            databaseCustomer[stt-1].setPhoneNumber(temp);
+            flag = 1;
+            break;
+        case 3:
+            system("cls");
+            cout << "Nhap dia chi: ";
+            cin.ignore();
+            getline(cin, temp);
+            databaseCustomer[stt-1].setAddress(temp);
+            flag = 1;
+            break;
+        case 0:
+            manageCustomer(databaseRoom, databaseCustomer);
+            return;
+            break;
+        default:
+            break;
+        }
+        if (flag){
+            cout << "Chinh sua thanh cong\n\n";
+            cout << "1: Tiep tuc\n0:Quay lai\n";
+            cout << "Nhap lua chon: \n";
+            cin >> key1;
+            if (key1 == 1){
+                editInfoCustomer(databaseRoom, databaseCustomer);
+                return;
+            }else{
+                manageCustomer(databaseRoom, databaseCustomer);
+                return;
+            }
+        }
+    }
+
+}
+
+void deleteInfoCustomer(vector<Room>& databaseRoom, vector<Customer>& databaseCustomer){
+    system("cls");
+    int stt, i;
+    int key;
+    string key1;
+    bool flag = 0;
+    bool continueEdit = 1;
+    string temp;
+    cout << "Nhap STT khach hang: ";
+    cin >> stt;
+    cout << "1:Chan chan xoa\n0:Quay lai\n\n";
+    while (1){
+        cout << "Nhap lua chon: \n";
+        cin >> key;
+        switch (key)
+        {
+        case 1:
+            for (i = 0; i < databaseRoom.size(); i++){
+                if (databaseRoom[i].getRoomNumber() == databaseCustomer[stt].getIDRoom()) break;
+            }
+            databaseRoom[i].checkOut();
+            databaseCustomer.erase(databaseCustomer.begin() + stt - 1);
+            system("cls");
+            cout << "Xoa thong tin thanh cong!!!\n";
+            cout << "Nhan phim bat ki de quay lai\n";
+            cin >> key1;
+            return;
+            break;
+        case 0:
+            deleteInfoCustomer(databaseRoom, databaseCustomer);
+            return;
+            break;
+        default:
+            break;
+        }
+    }
+
+    
+}
+//Main Function---------------------------------------------------------------
 void bookRoom(vector<Room>& databaseRoom, vector<Customer>& databaseCustomer){
     system("cls");
     int i;
     int columnWidth = 20;
+    string out_prog;
     while (1){
         int key = -1;
         cout << "----------------Dat phong---------------------\n\n";
@@ -162,7 +292,7 @@ void bookRoom(vector<Room>& databaseRoom, vector<Customer>& databaseCustomer){
             system("cls");
             cout << "----------------Tien hanh dat phong---------------------\n\n";
             if (databaseRoom.size() == 0){
-                cout << "Hien tai khong co phong, vui long nhan 0 de quay lai thiet lap phong\n";
+                cout << "Hien tai khong co phong\n";
                 while (1){
                     cout << "Nhan so bat ky de quay lai: ";
                     int key1;
@@ -183,9 +313,9 @@ void bookRoom(vector<Room>& databaseRoom, vector<Customer>& databaseCustomer){
                 cout << "1:Nhap so phong\n";
                 cout << "0:Quay lai\n\n";
                 int key3;
-                cin >> key3;
                 while (1){
                     cout << "Nhap lua chon: ";
+                    cin >> key3;
                     switch (key3)
                     {
                     case 1:
@@ -199,6 +329,9 @@ void bookRoom(vector<Room>& databaseRoom, vector<Customer>& databaseCustomer){
                         if (databaseRoom[i].isBookRoom()){
                             system("cls");
                             cout << "Phong nay da duoc dat!!!\n";
+                            cout << "Nhan phim bat ky de quay lai: ";
+                            cin >> out_prog;
+
                         }else{
                             system("cls");
                             cout << "----------------Tien hanh dat phong---------------------\n\n";
@@ -208,11 +341,11 @@ void bookRoom(vector<Room>& databaseRoom, vector<Customer>& databaseCustomer){
                             getline(cin, name);
                             cout << "Nhap sdt: ";
                             string sdt;
-                            cin.ignore();
+                            //cin.ignore();
                             getline(cin, sdt);
                             cout << "Nhap dia chi: ";
                             string address;
-                            cin.ignore();
+                            //cin.ignore();
                             getline(cin, address);
                             Customer customerTemp(name, sdt, address, numberRoomTemp);
                             databaseCustomer.push_back(customerTemp);
@@ -259,6 +392,52 @@ void bookRoom(vector<Room>& databaseRoom, vector<Customer>& databaseCustomer){
     }
 }
 
+void manageCustomer(vector<Room>& databaseRoom, vector<Customer>& databaseCustomer){
+    system("cls");
+    int i;
+    int columnWidth = 20;
+    int key = -1;
+    cout << "----------------Quan li khach hang---------------------\n\n";
+    cout << setw(columnWidth) << left << "STT";
+    cout << setw(columnWidth) << left << "ID Phong";
+    cout << setw(columnWidth) << left << "Ho va ten";
+    cout << setw(columnWidth) << left << "So dien thoai";
+    cout << setw(columnWidth) << left << "Dia chi";
+    cout << endl;
+    for (i = 0; i < databaseCustomer.size(); i++){
+        cout << setw(columnWidth) << left << i + 1;
+        cout << setw(columnWidth) << left << databaseCustomer[i].getIDRoom();
+        cout << setw(columnWidth) << left << databaseCustomer[i].getName();
+        cout << setw(columnWidth) << left << databaseCustomer[i].getPhoneNumber();
+        cout << setw(columnWidth) << left << databaseCustomer[i].getAddress();
+        cout << endl;
+    }
+
+    cout << endl;
+    cout << "1:Chinh sua thong tin\n";
+    cout << "2:Xoa thong tin\n";
+    cout << "0:Quay lai\n\n";
+    while (1){
+        cout << "Nhap lua chon: ";
+        cin >> key;
+        switch (key)
+        {
+        case 1:
+            editInfoCustomer(databaseRoom, databaseCustomer);
+            return;
+            break;
+        case 2:
+            deleteInfoCustomer(databaseRoom, databaseCustomer);
+            return;
+            break;
+        case 0:
+            return;
+            break;
+        default:
+            break;
+        }
+    }
+}
 
 int main(){
     system("cls");
@@ -279,7 +458,7 @@ int main(){
                 bookRoom(databaseRoom, databaseCustomer);
                 break;
             case CUSTOMER_MANAGE:
-                
+                manageCustomer(databaseRoom, databaseCustomer);
                 break;
             case STAFF_MANAGE:
                 
